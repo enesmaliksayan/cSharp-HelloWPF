94f1c4ae35d226dca5c97c6fb944c8d1926a07da
Enes Malik Sayan <enesmaliksayan@gmail.com>
2018-05-23 15:47:50 +0300

    play with items, item communication test

--- HelloWPF/MainWindow.xaml.cs
+++ HelloWPF/MainWindow.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,29 +20,56 @@ namespace HelloWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        ArrayList lenghtChecked = new ArrayList();
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-
-        }
 
         private void ApplyButton_Click(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show($"Description text is: {this.DescriptionText.Text}");
         }
 
         private void ResetButton_Click(object sender, RoutedEventArgs e)
         {
-
+            this.AssemblyCheckbox.IsChecked = this.DrillCheckbox.IsChecked = this.FoldCheckbox.IsChecked = this.LaserCheckbox.IsChecked
+                = this.LatheCheckbox.IsChecked = this.PlasmaCheckbox.IsChecked = this.PurchaseCheckbox.IsChecked
+                = this.RollCheckbox.IsChecked = this.SawCheckbox.IsChecked = this.WeldCheckbox.IsChecked = false;
         }
 
         private void RefreshButton_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Checkbox_Checked(object sender, RoutedEventArgs e)
+        {
+            lenghtChecked.Add((string)((CheckBox)sender).Content);
+            this.LengthText.Text = "";
+            foreach (object o in lenghtChecked)
+            {
+                this.LengthText.Text += (string)o + " ";
+            }
+        }
+
+        private void Checkbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            lenghtChecked.Remove((string)((CheckBox)sender).Content);
+            this.LengthText.Text = "";
+            foreach (object o in lenghtChecked)
+            {
+                this.LengthText.Text += (string)o + " ";
+            }
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string item = (string)((ComboBoxItem)((ComboBox)sender).SelectedValue).Content;
+            StatusText.Text = item;
         }
     }
 }
